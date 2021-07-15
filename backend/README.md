a85ec1b5a33f95c3698997f325f350e26d5c366c
Minsung <sns2831@gmail.com>
2021-07-16 03:57:18 +0900

    testffff

--- backend/README.md
+++ backend/README.md
@@ -3,3 +3,4 @@ f
 dd
 sdf
 d
+f
--- backend/pick-git/src/main/java/com/woowacourse/pickgit/PickGitApplication.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/PickGitApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PickGitApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PickGitApplication.class, args);
     }
